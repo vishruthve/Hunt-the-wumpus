4b34e9e3a78f9be9ba58be5c007445b2d5c838f8
Raydawg123 <2025728@apps.nsd.org>
2024-05-10 13:54:59 -0700

    hello

--- src/main/java/Cave.java
+++ src/main/java/Cave.java
@@ -70,7 +70,6 @@ public class Cave extends Base{
 			return CELLID+" "+XPOS+" "+YPOS+" "+Arrays.toString(NEIGHBORIDS)+" "+SHAPE+" "+rotation;
 		}
 
-
 		
 
 		
--- src/main/java/Gui.java
+++ src/main/java/Gui.java
@@ -1,9 +1,15 @@
 //Rayhan 
 // 3/8/34
 
+import java.swing.JFrame;
+import java.awt.FlowLayout;
 
 public class Gui {
-    public Gui(){
-
+    public Gui(String cave){
+        
+        System.out.println(cave);
+        setTitle("cave");
+        
+        
     }
 }
