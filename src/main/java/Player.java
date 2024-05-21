a264dd39927a54ce9ce043456f5bb0bf8147047c
Akshay Murthy <1109268@apps.nsd.org>
2024-05-21 13:47:36 -0700

    deleted rajeev's "hey"

---
+++ src/main/java/Player.java
@@ -0,0 +1,7 @@
+public class Player{ 
+    private boolean death = false;
+    private int num_arrows; 
+    public Player (boolean death) {
+        this.death = death; 
+    }
+}
--- src/main/java/Trivia.java
+++ src/main/java/Trivia.java
@@ -34,8 +34,4 @@ public class Trivia {
         numArrows --; 
     }
 
-
-system.out.println("hey");
-
-
 }
