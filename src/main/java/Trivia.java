821002004f4372e8b58eec8304a786ffb533fbe7
vishruthve <1120236@apps.nsd.org>
2024-05-31 14:25:08 -0700

    kdjhf

--- src/main/java/Trivia.java
+++ src/main/java/Trivia.java
@@ -2,33 +2,40 @@
 
 //Akshay
 import java.util.ArrayList;
- 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Trivia {
-    ArrayList<String> Trivia_Questions = new ArrayList<String>(); 
+    String Trivia_Question = new String(); 
     ArrayList<String> Trivia_Answers = new ArrayList<String>();
-    int index = (int) Math.random() * Trivia_Questions.size();
+    ArrayList<String> Questions = new ArrayList<String>();
+    int correctIndex;
+    int randomIndex = (int)( Math.random() * 14 );
     //Object Arrow = new Object(); 
     int numArrows; 
-    public Trivia() {
-        //filler
-        Trivia_Questions.add("In what country did the first Starbucks open outside of North America?");
-        Trivia_Questions.add("What does CODA stand for?");
-        Trivia_Questions.add("In a website browser address bar, what does “www” stand for?");
-        
-        Trivia_Answers.add("Japan");
-        Trivia_Answers.add("Child of Deaf Adults");
-        Trivia_Answers.add("World Wide Web");
+    public Trivia() throws FileNotFoundException, java.text.ParseException{
+        //filler*
+        Scanner s = new Scanner(new File("C:\\Git6\\Hunt-the-wumpus\\src\\main\\java\\Wumpus_Trivia - Sheet1.tsv"));
+        while(s.hasNextLine()){
+            
+            Questions.add(s.nextLine());
+            
+
         //add more later 
+        }
+        String[] arr = s.nextLine().split("\t");
+        Trivia_Question = arr[0];
+        correctIndex = Integer.parseInt(arr[1]);
+        Trivia_Answers.add(arr[2]);
+        Trivia_Answers.add(arr[3]);
+        Trivia_Answers.add(arr[4]);
+        Trivia_Answers.add(arr[5]);
     }
-    public String getTrivia() {
 
-        return Trivia_Questions.get(index); 
-
-    }
-    public int checkAnswer(String guess) {
-        if (Trivia_Answers.get(index).equals(guess)) numArrows++; 
-        return numArrows;
+    public boolean checkAnswer(int guess) {
+        return guess == correctIndex;
     }
 
     public void useArrows() {
