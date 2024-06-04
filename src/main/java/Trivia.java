3a912d95d50851e28ac23dee777d7d235187d5fe
Dowotdashdotdot <dowotdashdodot@gmail.com>
2024-06-04 13:47:49 -0700

    swag trivia

--- src/main/java/Trivia.java
+++ src/main/java/Trivia.java
@@ -8,13 +8,10 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Trivia {
-    String Trivia_Question = new String(); 
+    String Trivia_Question; 
     ArrayList<String> Trivia_Answers = new ArrayList<String>();
     ArrayList<String> Questions = new ArrayList<String>();
     int correctIndex;
-    int randomIndex = (int)( Math.random() * 14 );
-    //Object Arrow = new Object(); 
-    int numArrows; 
     public Trivia() throws FileNotFoundException, java.text.ParseException{
         //filler*
         Scanner s = new Scanner(new File("src/main/java/trivia/Wumpus_Trivia - Sheet1.tsv"));
@@ -27,22 +24,21 @@ public class Trivia {
         //add more later 
         }
         s.close();
-        String[] arr = Questions.get(0).split("\t");
+        getNewTrivia();
+    }
+
+    public void getNewTrivia(){
+        String[] arr = Questions.get((int)(Math.random()*Questions.size())).split("\t");
         Trivia_Question = arr[0];
         correctIndex = Integer.parseInt(arr[1]);
-        Trivia_Answers.add(arr[2]);
-        Trivia_Answers.add(arr[3]);
-        Trivia_Answers.add(arr[4]);
-        Trivia_Answers.add(arr[5]);
+        Trivia_Answers.clear();
+        for(int i=2;i<arr.length;i++) Trivia_Answers.add(arr[i]);
     }
 
     public boolean checkAnswer(int guess) {
         return guess == correctIndex;
     }
 
-    public void useArrows() {
-        numArrows --; 
-    }
 
 
 }
